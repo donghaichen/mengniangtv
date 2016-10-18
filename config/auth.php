b82654333e848a1d2e7bea64490141d20b759f1d
donghaichen <384560806@qq.com>
2016-10-18 08:13:15 +0800

    auth.config

---
+++ config/auth.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 身份认证密钥与盐。
+ * User: donghai
+ * Date: 2016/10/18
+ * Time: 01:47
+ */
+return[
+    'key' =>[
+        'AUTH' => env('KEY_AUTH'),
+        'SECURE_AUTH' => env('KEY_SECURE_AUTH'),
+        'LOGGED_IN' => env('KEY_LOGGED_IN'),
+        'NONCE' => env('KEY_NONCE')
+    ],
+    'salt' =>[
+        'AUTH' => env('SALT_KEY_AUTH'),
+        'SECURE_AUTH' => env('SALT_SECURE_AUTH'),
+        'LOGGED_IN' => env('SALT_LOGGED_IN'),
+        'NONCE' => env('SALT_NONCE')
+    ]
+];
